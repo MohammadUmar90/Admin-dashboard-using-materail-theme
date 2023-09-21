b2a083485e98f726b0334d31b3e9550ffd1bb551
unknown <you@example.com>
2023-09-21 10:56:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import TopBar from "./screen/global/TopBar";
+import { ColorModeContext,useMode } from "./theme";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import Dashboard from "./screen/dashboard";
+import Sidebar from "./screen/global/Sidebar";
+import Team from "./screen/team";
+import Contacts from "./screen/contacts";
+import Bar from "./screen/bar";
+import Line from "./screen/line";
+import Pie from "./screen/pie";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [theme, colorMode] = useMode();
+  return (<ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <div className="app">
+            <Sidebar />
+            <main className="content">
+              <TopBar />
+              <Routes>
+                <Route path="/" element={<Dashboard/>} />
+                <Route path="/team" element={<Team/>} />
+                <Route path="/contacts" element={<Contacts/>} />
+                <Route path="/bar" element={<Bar/>} />
+                <Route path="/line" element={<Line/>} />
+                <Route path="pie" element={<Pie/>} />
+              </Routes>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
